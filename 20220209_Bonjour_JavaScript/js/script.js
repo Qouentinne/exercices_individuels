015e5831f0dce25b73a46c8c0d825af097a2c5ec
Qouentinne <quent1_jarry@hotmail.com>
2022-02-09 16:24:55 +0100

    feat(js): Add askName() and askAge() functions

--- 20220209_Bonjour_JavaScript/js/script.js
+++ 20220209_Bonjour_JavaScript/js/script.js
@@ -1,1 +1,16 @@
+function askName(){
+    let nom = prompt("Bonjour, vous, comment vous appelez-vous ?")
+    let salut = "👋 Bonjour " + nom
+    let newH2 = document.createElement("h2", salut)
+    newH2.innerText = salut
+    document.querySelector("body").append(newH2)
+}
 
+function askBirthYear(){
+    annee = prompt("Merci, en quelle année êtes-vous né(e)?")
+    age = new Date().getFullYear() - Number(annee)
+    document.body.innerHTML += "<h3>Vous avez " + age + " ans</h3>"
+}
+
+askName()
+askBirthYear()
