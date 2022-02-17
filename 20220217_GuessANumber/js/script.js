fc0b2f5af02cb660b333707dae7e30a17c723ded
Qouentinne <quent1_jarry@hotmail.com>
2022-02-17 10:36:32 +0100

    feat : ajout de fonction gamePlay pour répéter la boucle de jeu

--- 20220217_GuessANumber/js/script.js
+++ 20220217_GuessANumber/js/script.js
@@ -1,9 +1,29 @@
-let givenNumber = parseInt(prompt("Renseignez un nombre"))
-
-function didIWin(givenNumber){
-    if (givenNumber == 22) {
-        console.log("Bravo ! Vous avez deviné le nombre")
-    } else {
-        
-    }
+function askNumber(){
+    return parseInt(prompt("Renseignez un nombre"))
 }
+
+function didIWin(gNumber){
+    numberToGuess = 22
+    if (gNumber == numberToGuess) {
+        console.log("Bravo ! Vous avez deviné le nombre")
+        return true
+    } else {
+        if (gNumber < numberToGuess){
+            console.log("Plus grand")
+        } else {
+            console.log("Plus petit")
+        }
+        return false
+        }       
+    }
+
+
+function gamePlay(){
+    while (didIWin(askNumber()) == false){
+        didIWin(askNumber())
+    }
+
+}
+
+
+gamePlay()
