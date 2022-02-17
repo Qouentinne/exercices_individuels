dd2329d7ead3c75779c4b48cf9b6eaff1c307c4f
Qouentinne <quent1_jarry@hotmail.com>
2022-02-17 10:17:36 +0100

    feat : ajout de prompt et fonction de comparaison

--- 20220217_GuessANumber/js/script.js
+++ 20220217_GuessANumber/js/script.js
@@ -1,1 +1,9 @@
+let givenNumber = parseInt(prompt("Renseignez un nombre"))
 
+function didIWin(givenNumber){
+    if (givenNumber == 22) {
+        console.log("Bravo ! Vous avez deviné le nombre")
+    } else {
+        
+    }
+}
