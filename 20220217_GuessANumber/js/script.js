f2e0cae0991b39474f25adc38b6db608420b21db
Qouentinne <quent1_jarry@hotmail.com>
2022-02-18 11:04:33 +0100

    feat: trials history + counter is now shown

--- 20220217_GuessANumber/js/script.js
+++ 20220217_GuessANumber/js/script.js
@@ -3,6 +3,7 @@ let exerciceMaxValue = 50
 let rangeString = `entre ${exerciceMinValue} et ${exerciceMaxValue}`
 let numberToGuess = NaN
 let attempts = 0
+let gameIsOn = false
 
 
 
@@ -29,16 +30,18 @@ function startGame(){
     })
 
     document.querySelector("body").appendChild(answ_input)
-    document.querySelector("body").appendChild(returnButton) 
+    document.querySelector("body").appendChild(returnButton)
+    document.querySelector("body").appendChild(document.createElement("p"))   
     document.querySelector("body").appendChild(document.createElement("p"))
-
     
+    gameIsOn = true
 
     return
 }
 
 function returnInfo(s=""){
-   document.querySelector("p").innerHTML = s
+    document.getElementsByTagName("p")[0].innerHTML = `${attempts}e essai` 
+    document.getElementsByTagName("p")[1].innerHTML += s+"<br>"
 }
 
 function guessNumber(){
@@ -50,7 +53,7 @@ function askNumberToGuess(){
 }
 
 function checkNumberRange(n){
-    if (n > exerciceMinValue  && n < exerciceMaxValue){
+    if (n >= exerciceMinValue  && n <= exerciceMaxValue){
         return true
     } else {
         console.log("Le nombre doit être compris " + rangeString)
@@ -59,21 +62,25 @@ function checkNumberRange(n){
 }
 
 function didIWin(gNumber, numberToGuess){
-    if (gNumber == numberToGuess) {
-        console.clear()
-        console.log("Bravo ! Vous avez deviné le nombre")
-        returnInfo("Bravo ! Vous avez deviné le nombre")
-        return true
-    } else {
-        if (gNumber < numberToGuess){
-            console.log("Plus grand")
-            returnInfo("Plus grand")
+    if (gameIsOn == true){
+        attempts ++
+        document.getElementsByName("answer").value = ""
+        if (gNumber == numberToGuess) {
+            console.clear()
+            console.log("Bravo ! Vous avez deviné le nombre")
+            returnInfo("<b>Bravo ! Vous avez deviné le nombre en " +attempts+ " essais</b>")
+            gameIsOn = false
         } else {
-            console.log("Plus petit")
-            returnInfo("Plus petit")
-        }
-        return false
-        }       
+            if (gNumber < numberToGuess){
+                console.log("Le nombre cherché est plus grand que "+gNumber)
+                returnInfo("Le nombre cherché est plus grand que "+gNumber)
+            } else {
+                console.log("Le nombre cherché est plus petit que "+gNumber)
+                returnInfo("Le nombre cherché est plus petit que "+gNumber)
+            }
+            return false
+            }  
+        }     
     }
 
 
