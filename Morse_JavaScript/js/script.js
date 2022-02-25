d470fe19735a174b3f944978928bd9d3f243704f
Qouentinne <quent1_jarry@hotmail.com>
2022-02-25 11:56:14 +0100

    feat step 1-3

--- Morse_JavaScript/js/script.js
+++ Morse_JavaScript/js/script.js
@@ -1,1 +1,76 @@
+const latinToMorse = {
+	'A':'.-',
+	'B':'-...',
+	'C':'-.-.',
+	'D':'-..',
+	'E':'.',
+	'F':'..-.',
+	'G':'--.',
+	'H':'....',
+	'I':'..',
+	'J':'.---',
+	'K':'-.-',
+	'L':'.-..',
+	'M':'--',
+	'N':'-.',
+	'O':'---',
+	'P':'.--.',
+	'Q':'--.-',
+	'R':'.-.',
+	'S':'...',
+	'T':'-',
+	'U':'..-',
+	'V':'...-',
+	'W':'.--',
+	'X':'-..-',
+	'Y':'-.--',
+	'Z':'--..'
+}
 
+const morseToLatin = {
+    '-': "T",
+    '--': "M",
+    '---': "O",
+    '--.': "G",
+    '--.-': "Q",
+    '--..': "Z",
+    '-.': "N",
+    '-.-': "K",
+    '-.--': "Y",
+    '-.-.': "C",
+    '-..': "D",
+    '-..-': "X",
+    '-...': "B",
+    '.': "E",
+    '.-': "A",
+    '.--': "W",
+    '.---': "J",
+    '.--.': "P",
+    '.-.': "R",
+    '.-..': "L",
+    '..': "I",
+    '..-': "U",
+    '..-.': "F",
+    '...': "S",
+    '...-': "V",
+    '....': "H"
+  }
+
+function getLatinCharacterList(string) {
+    return Array.from(string)   
+}
+
+function translateLatinCharacter(char) {
+    return latinToMorse[char]
+}
+
+function encode(array) {
+    let encodedArr = []
+    for (let char of array){
+        if ((/[a-z]+/).test(char)){
+            char=char.toUpperCase()
+        }
+        encodedArr.push(translateLatinCharacter(char))
+    }
+    return encodedArr
+}
