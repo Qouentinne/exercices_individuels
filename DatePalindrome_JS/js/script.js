0e8a150a8c01e7b2ed3befbc7c687186bd5f38be
Qouentinne <quent1_jarry@hotmail.com>
2022-03-10 15:00:18 +0100

    feat: isValid isPalindrome nextPalindromes

--- DatePalindrome_JS/js/script.js
+++ DatePalindrome_JS/js/script.js
@@ -14,3 +14,45 @@ let isValidDate = function(gDate){
     }
     return true
 }
+
+function isPalindrome(gDate){
+    //check valid date
+    if (isValidDate(gDate)){
+        const arr1 = gDate.match(/\d/g) //create an array w/ only numbers
+        let arr2 = arr1.slice()
+        arr2.reverse() //reverse array
+        for (let i=0; i < arr1.length; i++){ // compare elements of arrays 1 by 1, comparing arrays directly does not work
+            if(arr1[i] != arr2[i]){
+                return false
+            }
+        }
+        return true
+    }else{console.log("Date non valide")}
+}
+
+function formatLowNumbers(n){
+    if (parseInt(n)<10){
+        return "0"+n
+    }
+    return n
+}
+
+function dateToString(date){
+    return formatLowNumbers(date.getDate().toString()) + "/" + formatLowNumbers((date.getMonth()+1).toString()) + "/" + date.getFullYear().toString()
+} 
+
+function getNextPalindromes(x){
+    let date = new Date()
+    results = []
+    let dateString=""
+    while (results.length<x){
+        dateString = dateToString(date)
+        if(isPalindrome(dateString)) {
+            results.push(dateString)
+        }
+    date.setDate(date.getDate()+1)
+    }
+    results.forEach(d => {
+        console.log(d)
+    });
+}
